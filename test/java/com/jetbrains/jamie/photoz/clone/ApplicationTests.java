f7eb4e4342ab350f4f405e9e6d1018e30c42adcb
Jamire44 <2003doyle@gmail.com>
2023-12-05 13:43:31 +0000

    Initial commit

---
+++ test/java/com/jetbrains/jamie/photoz/clone/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.jamie.photoz.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
